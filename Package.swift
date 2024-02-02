7785176306de61349df0190979e7c5e6eece9a82
mohit <mohit@inkwired.com>
2024-02-02 16:55:18 +0530

    Initial Release

--- Package.swift
+++ Package.swift
@@ -9,15 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PushAlert-XCFramework",
-            targets: ["PushAlert-XCFramework"]),
+            targets: ["PushAlert"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "PushAlert-XCFramework"),
-        .testTarget(
-            name: "PushAlert-XCFrameworkTests",
-            dependencies: ["PushAlert-XCFramework"]),
+        .binaryTarget(name: "PushAlert", url: "https://github.com/InkWired/pushalert-ios-sdk/releases/download/v0.0.90/PushAlert.xcframework.zip", checksum: "bd4d3f620b845bdb146a73806b9c9c897a0d83c702e35c0a584d5c7b8ef5e9ea")
     ]
 )
