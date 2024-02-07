5da6a1224a87ecc6deac8ced6a1581c833cd95b0
mohit <mohit@inkwired.com>
2024-02-07 17:55:37 +0530

    - Changed APIs url specific to iOS - Synced subscription process

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "PushAlert", url: "https://github.com/InkWired/pushalert-ios-sdk/releases/download/v0.0.90/PushAlert.xcframework.zip", checksum: "bd4d3f620b845bdb146a73806b9c9c897a0d83c702e35c0a584d5c7b8ef5e9ea")
+        .binaryTarget(name: "PushAlert", url: "https://github.com/InkWired/pushalert-ios-sdk/releases/download/v0.0.91/PushAlert.xcframework.zip", checksum: "d0b707201a7b05b3aa4cea2a125ec549030e8adcb9d57d941df39ea6fdec8576")
     ]
 )
